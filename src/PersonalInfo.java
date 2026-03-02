f120060273213c420aa157dfac2aad0c393de124
Charan Challa <challacharan14@gmail.com>
2026-03-02 15:33:11 +0530

    Added PersonalInfo program

---
+++ src/PersonalInfo.java
@@ -0,0 +1,12 @@
+public class PersonalInfo {
+    public static void main(String[] args){
+        String name = "Charan";
+        int age = 22;
+        String city = "Hyderabad";
+        System.out.println("Personal Information");
+        System.out.println("---------------");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+    }
+}
