9a05b17c7dee0afbab2257c1db44c0fc5a4ce894
Charan Challa <challacharan14@gmail.com>
2026-03-05 11:48:34 +0530

    Added one dimensional array program

---
+++ src/OneDArray.java
@@ -0,0 +1,9 @@
+public class OneDArray  {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Elements in the array:");
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
